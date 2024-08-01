3583c9e4963e0659e635ed0fbfb701eb9ac5e5a8
Sahar Guebsi <154086720+06sahar06@users.noreply.github.com>
2024-08-01 01:02:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_groq import ChatGroq
 chat = ChatGroq(
     temperature=0,
     model="llama3-70b-8192",
-    api_key="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" 
+    api_key="insert your api key here" 
 )
 
 system = "You will be provided with RFP texts (job descriptions) from a client, and your task is to anonymize the content by removing any details related to the company, such as the company's name, department, and contact information. The anonymized text should retain all essential job requirements, qualifications, and responsibilities while ensuring that no identifying company-specific information"
